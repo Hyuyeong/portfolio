5631ad871159d257deae5881e2fa51455114c83e
Hyuyeong <choibeny@gmail.com>
2022-06-17 18:29:57 +1200

    starting portfolios

--- src/components/Portfolios.jsx
+++ src/components/Portfolios.jsx
@@ -1,7 +1,25 @@
 //import {useState} from 'react'
 //import styles from './Portfolios.module.css'
 
+import img1 from '../assets/pokemon.png';
+
 const Portfolios = props => {
-  return <div> pp </div>;
+  return (
+    <div className="portfolios">
+      <div className="portfolio">
+        <img src={img1} alt="" />
+
+        <div>
+          <div>text1</div>
+          <div>text2</div>
+          <div>text3</div>
+          <div>text4</div>
+          <div>text5</div>
+          <div>text6</div>
+          <div>text7</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Portfolios;
